129f90e71b95124db574805b2fe7589139c45ca5
001kpp <alihankuchmenov@gmail.com>
2024-12-07 20:43:02 +0300

    vk_fix

--- bot.py
+++ bot.py
@@ -1,18 +1,15 @@
+from vkwave.bots import SimpleLongPollBot
 from config import *
-
-from vkbottle import PhotoMessageUploader
-
-from vkbottle import API
-from vkbottle.bot import Bot, Message
-
+import vkwave
 import asyncio
 
-api = API(bot_token)
-bot = Bot(api=api)
+async def main():
+    bot = SimpleLongPollBot(client=vkwave.client.AIOHTTPClient(loop=asyncio.get_event_loop()), tokens=bot_token, group_id=group_ig)
+    @bot.message_handler(bot.text_filter("начало"))
+    async def handle(event: bot.SimpleBotEvent) -> str:
+        user_data = (await event.api_ctx.users.get(user_ids=event.object.object.message.from_id)).response[0]
+        await event.answer(message=user_data) 
+    asyncio.create_task(bot.run())
+    while True: await asyncio.sleep(0)    
 
-@bot.on.message(text=["//гифка", "//Гифка", "ЮС гифка", "ЮС Гифка", "юс Гифка", "Юс гифка", "Юс Гифка", "Юс ГИФКА" "юс гифка", "ЮС ГИФКА",  "//ГИФКА"])
-async def get_handler(msg: Message):
-    users_info = await bot.api.users.get(msg.from_id)
-    await msg.answer("{}".format(users_info))
-
-bot.run_forever()
+asyncio.run(main())
