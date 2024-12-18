3768d4d788e88b5ee8c6bdce825e7903677fccc3
001kpp <alihankuchmenov@gmail.com>
2024-12-18 19:08:21 +0300

    .

--- GIF_ex.py
+++ GIF_ex.py
@@ -1,26 +1,26 @@
 from PIL import Image
 from loguru import logger
 
-def gif_encs(path, paths_to_save:list, frames_to_cut:int=3) ->list:
+def gif_encs(path, paths_to_save: list, frames_to_cut: int = 3) -> list:
+    if len(paths_to_save) != frames_to_cut:
+        raise ValueError("Количество путей для сохранения должно совпадать с количеством кадров для вырезки.")
+
     with Image.open(path) as gif:
         frames_count = gif.n_frames
-        i = 0
-        j=0
-        cnt = 0  
         logger.debug(f"frames in file: {frames_count}")
-        try:    
-            while cnt < frames_to_cut and i < frames_count:
-                    gif.seek(i)
-                    i += frames_count // (frames_to_cut - 1)
-                    cnt += 1
-                    gif.seek(frames_count - 1)
-                    frame = gif.convert('RGB')
-                    frame.save(paths_to_save[j])
-                    j+=1
-            logger.success(f"images_save_complete from {paths_to_save}")          
-            return paths_to_save
-        except  ValueError as e:
-            logger.error(f"{e} \n paths_to_save:{paths_to_save}")
+        
+
+        if frames_count < frames_to_cut:
+            raise logger.error(f"В файле недостаточно кадров ({frames_count}) для вырезки {frames_to_cut} кадров.")
+
+        for i in range(frames_to_cut):
+            index = round((i / (frames_to_cut - 1)) * (frames_count - 1))
+            gif.seek(index)
+            frame = gif.convert('RGB')
+            frame.save(paths_to_save[i])
+
+        logger.success(f"Images saved to: {', '.join(paths_to_save)}")
+        return paths_to_save
 
 
 
